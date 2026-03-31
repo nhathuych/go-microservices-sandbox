72b4810cfa85029a7f013d9c2c86154a86be06fe
nhathuych <nhathuych@gmail.com>
2026-03-31 18:45:16 +0700

    add event and consumer to Broker service and connect to RabbitMQ

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"os"
 	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const serverPort = "80"
 
-type Config struct{}
+type Config struct {
+	RabbitMQ *amqp.Connection
+}
 
 func main() {
-	app := Config{}
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+	log.Println("Connected to RabbitMQ!")
+
+	app := Config{
+		RabbitMQ: rabbitConn,
+	}
 
 	log.Printf("Broker service is starting on port %s\n", serverPort)
 
@@ -27,4 +43,34 @@ func main() {
 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("listen: %s\n", err)
 	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			log.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to RabbitMQ!")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+
+	return connection, nil
 }
